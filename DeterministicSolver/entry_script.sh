f8b3fd5ec3d677441c412b6718719f78dce18fdf
Josh Dawes <joshradawes@gmail.com>
2024-01-13 01:01:56 +1300

    Small change to the lambda entry point script to get it working on AWS

--- DeterministicSolver/entry_script.sh
+++ DeterministicSolver/entry_script.sh
@@ -3,5 +3,5 @@
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   exec /usr/local/bin/aws-lambda-rie /usr/bin/python -m awslambdaric --log-level "debug" "$@"
 else
-  exec /usr/local/bin/python -m awslambdaric "$@"
+  exec /usr/bin/python -m awslambdaric "$@"
 fi
