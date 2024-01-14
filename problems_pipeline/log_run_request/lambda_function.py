baebff7037746eaceeedc7dee6962d49cffad502
Josh Dawes <joshradawes@gmail.com>
2024-01-15 01:12:53 +1300

    Updated log_run_request to work with psycopg2 and correctly interpret the GET variables from the API gateway URL

--- problems_pipeline/log_run_request/lambda_function.py
+++ problems_pipeline/log_run_request/lambda_function.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import sys
-import psycopg
+import psycopg2
 import json
 import boto3
 from datetime import datetime
@@ -21,10 +21,10 @@ step_func_client = boto3.client('stepfunctions')
 
 # Connect to the database
 try:
-    conn = psycopg.connect(host=host, dbname=dbname, user=user, password=password, connect_timeout=10)
+    conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, connect_timeout=10)
     logging.info("Successfully connected to PostgreSQL.")
     
-except (Exception, psycopg.Error) as e:
+except (Exception, psycopg2.Error) as e:
     logger.error("ERROR: Failed to connect to PostgreSQL.")
     logger.error(e)
     sys.exit()
@@ -36,13 +36,16 @@ def lambda_handler(event, context):
             
         # Extract user's input data from their API call
         try:
+            print('event:', json.dumps(event))
+            print('queryStringParameters:', json.dumps(event['queryStringParameters']))
+            
             user_input = event['queryStringParameters']
-            expected_input = ('start', 'end', 'kwh', 'kw', 'node')
+            expected_input = ('start_time', 'end_time', 'kwh_to_charge', 'kw_charge_rate', 'node')
             if all(key in user_input for key in expected_input):
-                start_time = datetime.strptime(user_input.get('start'), '%Y-%m-%d %H:%M')
-                end_time = datetime.strptime(user_input.get('end'), '%Y-%m-%d %H:%M')
-                kwh_to_charge = float(user_input.get('kwh'))
-                kw_charge_rate = float(user_input.get('kw'))
+                start_time = datetime.strptime(user_input.get('start_time'), '%Y-%m-%d %H:%M:%S')
+                end_time = datetime.strptime(user_input.get('end_time'), '%Y-%m-%d %H:%M:%S')
+                kwh_to_charge = float(user_input.get('kwh_to_charge'))
+                kw_charge_rate = float(user_input.get('kw_charge_rate'))
                 node = user_input.get('node')
                 logger.info("Successfully extracted user input data.")
             else:
@@ -75,7 +78,7 @@ def lambda_handler(event, context):
             request_id = cur.fetchone()[0]
             logger.info(f"Successfully logged the run request in opt_requests table, for request_id: {request_id}.")
             
-        except (Exception, psycopg.Error) as e:
+        except (Exception, psycopg2.Error) as e:
             logger.error("ERROR: Failed to log the run request in opt_requests table.")
             logger.error(e)
             sys.exit()
@@ -91,7 +94,7 @@ def lambda_handler(event, context):
             )
             logger.info("Successfully triggered the step function to invoke the rest of the opt problems pipeline.")
             
-        except (Exception, psycopg.Error) as e:
+        except (Exception, psycopg2.Error) as e:
             logger.error("ERROR: Failed to trigger the step function for the opt problems pipeline.")
             logger.error(e)
             sys.exit()
